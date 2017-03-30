6dd3b8b3898576de80795f4110afe6053646f234
Kevin Yap <me@kevinyap.ca>
2017-03-30 02:44:15 -0700

    Add Git status output to exa aliases

--- source/aliases.sh
+++ source/aliases.sh
@@ -14,11 +14,11 @@ fi
 
 # Use exa in place of ls if installed
 if command -v exa >/dev/null 2>&1; then
-	alias l="exa -l"
+	alias l="exa -l --git"
 	alias ls="exa"
-	alias la="exa -la"
-	alias lt="exa -lRT"
-	alias lta="exa -laRT"
+	alias la="exa -la --git"
+	alias lt="exa -lRT --git"
+	alias lta="exa -laRT --git"
 else
 	# Use coloured output for `ls`
 	alias ls="command ls ${colorflag}"
